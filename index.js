a04558692dc83721e42b09b0a34716510cb110f3
EddieLo <eddarlostudy@yahoo.com>
2022-03-24 18:52:10 -0700

    questions for engineer/intern

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const team = [];
 
 const questions = () => {
     return inquirer.prompt ([
+// prompt manager questions
 // const addManager = [
     {
         name: 'role',
@@ -25,7 +26,7 @@ const questions = () => {
     {
         name: 'id',
         type: 'input',
-        message: 'What is your employee ID (Required)',
+        message: 'What is your employee ID: (Required)',
         validate: idInput => {
             if (idInput){
                 return true;
@@ -58,7 +59,101 @@ const questions = () => {
         choices: ['Add Engineer', 'Add Intern', 'All done!'],
         message: 'What you like to do next?'
     },
+    
 ]);
 };
+
+const addEngineer = [
+    {
+        name: 'name',
+        type: 'input',
+        message: 'Please enter engineer name'
+    },
+    {
+        name: 'id',
+        type: 'input',
+        message: 'Please enter engineer id: (Required)',
+        validate: engID => {
+            if (engID){
+                return true;
+            } else {
+                console.log('Please enter engineer ID');
+                return false;
+            }
+        }
+    },
+    {
+        name: "email",
+        type: 'input',
+        message: 'Please enter email address:'
+    },
+    {
+        name: 'github',
+        type: 'input',
+        message: ' Please enter engineers github (Required)',
+        validate: engGit => {
+            if (engGit){
+                return true;
+            }else {
+                console.log('Please provide engineer github');
+                return false;
+            }
+        }
+    },
+    {
+        name: 'followUp',
+        type: 'list',
+        choices: ['Add Engineer', 'Add Intern', 'All done!'],
+        message: 'What you like to do next?'
+    },
+
+];
+
+const addIntern = [
+    {
+        name: 'name',
+        type: 'input',
+        message: 'Please enter intern name'
+    },
+    {
+        name: 'id',
+        type: 'input',
+        message: 'Please enter intern id: (Required)',
+        validate: ternID => {
+            if (ternID){
+                return true;
+            } else {
+                console.log('Please enter intern ID');
+                return false;
+            }
+        }
+    },
+    {
+        name: "email",
+        type: 'input',
+        message: 'Please enter email address:'
+    },
+    {
+        name: 'school',
+        type: 'input',
+        message: ' Please enter interns school (Required)',
+        validate: ternSchool => {
+            if (ternSchool){
+                return true;
+            }else {
+                console.log('Please provide interns school');
+                return false;
+            }
+        }
+    },
+    {
+        name: 'followUp',
+        type: 'list',
+        choices: ['Add Engineer', 'Add Intern', 'All done!'],
+        message: 'What you like to do next?'
+    },
+
+];
+
 
 questions();
